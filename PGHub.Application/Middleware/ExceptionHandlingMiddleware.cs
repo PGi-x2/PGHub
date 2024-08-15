0c168ec197f70e3cc3e149ffe347d62e74318305
Porgras Andrei <porgrasandrei@yahoo.com>
2024-08-15 23:58:05 +0300

    Add ExceptionHandlingMiddleware and Newtonsoft.Json
    - Updated PGHub.Application. to include Newtonsoft.Json 13.0.3 for JSON serialization.
    - Added ExceptionHandlingMiddleware to handle HTTP request exceptions, log errors, and return JSON error responses.

---
+++ PGHub.Application/Middleware/ExceptionHandlingMiddleware.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace PGHub.Application.Middleware
+{
+    public class ExceptionHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<ExceptionHandlingMiddleware> _logger;
+
+        public ExceptionHandlingMiddleware(RequestDelegate next, ILogger<ExceptionHandlingMiddleware> logger)
+        {
+            _next = next;
+            _logger = logger;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An error occurred while processing the request.");
+                await HandleExceptionAsync(context, ex);
+            }
+        }
+
+        private static Task HandleExceptionAsync(HttpContext context, Exception exception)
+        {
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = new
+            {
+                StatusCode = context.Response.StatusCode,
+                Message = "Internal Server Error. Please try again later.",
+                ExceptionMessage = exception.Message // Optional
+            };
+
+            return context.Response.WriteAsync(JsonConvert.SerializeObject(response));
+        }
+    }
+}
